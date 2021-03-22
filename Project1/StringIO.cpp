f3c07403bb4667d33a68154f27e9e3dd1690c29b
skanwngud <skanwngud@gmail.com>
2021-03-22 18:13:45 +0900

    commit

--- Project1/StringIO.cpp
+++ Project1/StringIO.cpp
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-	char name[100];
-	char lang[200];
+	char name[100]; // name 변수 초기화
+	char lang[200]; // lang 변수 초기화
 
 	std::cout << "이름은 무엇입니까?";
 	std::cin >> name;
@@ -11,7 +11,7 @@ int main(void)
 	std::cout << "좋아하는 프로그래밍 언어는 무엇인가요?";
 	std::cin >> lang;
 
-	std::cout << "내 이름은 "<<name<<" 입니다.\n";
+	std::cout << "내 이름은 "<<name<<" 입니다.\n"; // 문자열 중간에 << << 로 변수를 넣어 출력
 	std::cout << "내가 제일 좋아하는 언어는 " << lang << " 입니다." << std::endl;
 
 	return 0;
